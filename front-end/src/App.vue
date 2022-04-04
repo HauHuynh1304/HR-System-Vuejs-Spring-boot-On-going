c6e1019cf3368ef4597a1fa6c225eaa5e18b9907
“HauHuynh1304” <hauhuynh1304@gmail.com>
2022-04-04 13:18:06 +0700

    deleted components

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,39 @@
 <template>
   <div id="app">
     <router-view />
+    <!-- you can add element-ui's tooltip -->
+    <el-tooltip placement="top" content="tooltip">
+      <back-to-top :custom-style="myBackToTopStyle" :visibility-height="300" :back-position="50" transition-name="fade" />
+    </el-tooltip>
+  </div>
   </div>
 </template>
 
 <script>
+import BackToTop from "@/components/BackToTop";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: { BackToTop },
+  data() {
+    return {
+      // customizable button style, show/hide critical point, return position
+      myBackToTopStyle: {
+        right: "50px",
+        bottom: "50px",
+        width: "40px",
+        height: "40px",
+        "border-radius": "4px",
+        "line-height": "45px",
+        background: "#e7eaf1",
+      },
+    };
+  },
+};
 </script>
+
+<style scoped>
+.placeholder-container div {
+  margin: 10px;
+}
+</style>
