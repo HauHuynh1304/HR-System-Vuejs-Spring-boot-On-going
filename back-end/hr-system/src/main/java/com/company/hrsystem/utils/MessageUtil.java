b704ccbf0793d26d207853cb70d95ab2f09ccfe0
“HauHuynh1304” <hauhuynh1304@gmail.com>
2022-12-27 16:57:18 +0700

    fix null message source

--- back-end/hr-system/src/main/java/com/company/hrsystem/utils/MessageUtil.java
+++ back-end/hr-system/src/main/java/com/company/hrsystem/utils/MessageUtil.java
@@ -4,9 +4,11 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 
 import com.company.hrsystem.config.SystemProperties;
 
+@Component
 public class MessageUtil {
 
 	private static MessageUtil instance;
